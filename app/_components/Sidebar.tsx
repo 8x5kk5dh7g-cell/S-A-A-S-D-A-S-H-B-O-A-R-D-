317ea76eeee4d901e0ea6e77e3c19e1eefe263d6
8x5kk5dh7g-cell <8x5kk5dh7g@privaterelay.appleid.com>
2026-01-16 00:57:17 -0300

    Update Sidebar.tsx

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+
 
 export default function BotPage() {
   const [nome, setNome] = useState("");
